1c880952c6c9104ea3f5c9099164e4d2aa523aa0
Tiburso <manuelmariamc@gmail.com>
2024-05-20 13:48:17 +0200

    Add seeding to the code for easier testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,48 +37,6 @@ const SensitiveTag = {
   color: BadgeColor.ORANGE,
 }
 
-// const variables: Variable[] = [
-//   {
-//     name: 'Food XP Multiplier',
-//     description:
-//       'How much XP users should gain per food point consumed or exceeded while in a hotel.',
-//     tags: [DangerousTag, PerformanceTag],
-//     value: {
-//       type: {
-//         type: VariableTypes.FREE_INPUT,
-//         inputType: FreeInputVariableTypes.FLOAT,
-//       },
-//       value: 3.21,
-//     },
-//   },
-//   {
-//     name: 'Pet Revival Time (Hours)',
-//     description:
-//       'How many hours after the pet dies the user can take to revive it.',
-//     tags: [DangerousTag],
-//     value: {
-//       type: {
-//         type: VariableTypes.FREE_INPUT,
-//         inputType: FreeInputVariableTypes.INTEGER,
-//       },
-//       value: 48,
-//     },
-//   },
-//   {
-//     name: 'OpenAI Model',
-//     description:
-//       'Model used for responding to PetBot users. Tune for better performance and cost saving.',
-//     tags: [SensitiveTag],
-//     value: {
-//       type: {
-//         type: VariableTypes.DROPDOWN,
-//         options: ['gpt-3.5-turbo', 'gpt-4.0-turbo', 'gpt-4o'],
-//       },
-//       value: 'gpt-3.5-turbo',
-//     },
-//   },
-// ]
-  
 export default async function Home() {
   const variables = await getVariables()  
 
@@ -86,29 +44,8 @@ export default async function Home() {
     <>
       <div className="h-10"/>
           <div className="flex flex-col gap-8">
-            {variables.map((variable) => {
-              // const v: Variable = {
-              //   name: variable.name,
-              //   description: variable.description as string,
-              //   tags: variable.tags.map((tag) => {
-              //     return {
-              //       name: tag.name,
-              //       color: tag.color,
-              //     }
-              //   }) as VariableTag[],
-              //   value: {
-              //     type: {
-              //       type: VariableTypes.FREE_INPUT,
-              //       inputType: FreeInputVariableTypes.FLOAT,
-              //     },
-              //     value: variable.value,
-              //   },
-              // }
-
-
-              return (
-                <VariableCard variable={variable} key={variable.id} />
-              )}
+            {variables.map((variable) => 
+              <VariableCard variable={variable} key={variable.id} />
             )}
         </div>
     </>
