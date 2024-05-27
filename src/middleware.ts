193271e6ab69eb062bd503c2e593feeadc78fd37
Tiburso <manuelmariamc@gmail.com>
2024-05-27 10:10:51 +0200

    Update middleware function for correct redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,21 @@
-// import { auth } from "@/auth"
+import { auth } from "@/auth"
+import { NextResponse } from "next/server"
 
-export { auth as middleware } from "@/auth"
+// export { auth as middleware } from "@/auth"
 
-// export default auth((req) => {
+export default auth((req) => {
 
-//     // If the user is not authenticated take them to the login page
-//     if (!req.auth) {
-//         // Get the base url
-//         const url = new URL(req.url)
+    // If the user is not authenticated take them to the login page
+    if (!req.auth) {
+        // Get the base url
+        const url = new URL(req.url)
 
-//         // Redirect to the login page
-//         const fullUrl = `${url.protocol}//${url.host}/login`
-//         return Response.redirect(fullUrl, 302)
-//     }
-// })
+        // Redirect to the login page
+        const fullUrl = `${url.protocol}//${url.host}/login`
+        return NextResponse.redirect(fullUrl)
+    }
+})
 
-// export const config = {
-//     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
-// }
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+}
