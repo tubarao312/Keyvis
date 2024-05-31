09747c5076a7250516f3fc078fb629c99992cda8
Tiburso <manuelmariamc@gmail.com>
2024-05-31 20:49:19 +0200

    Reenabled public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { authConfig } from '@/auth.config';
 
 const { auth } = NextAuth(authConfig);
 
-const PUBLIC_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/login"];
 
 export default auth((req) => {
     const { nextUrl } = req;
@@ -11,9 +11,9 @@ export default auth((req) => {
     const isAuthenticated = !!req.auth;
     const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
 
-    // if (isPublicRoute && isAuthenticated) {
-    //     return Response.redirect(new URL("/", nextUrl));
-    // }
+    if (isPublicRoute && isAuthenticated) {
+        return Response.redirect(new URL("/", nextUrl));
+    }
 
     if (!isAuthenticated && !isPublicRoute) {
         return Response.redirect(new URL("/login", nextUrl));
