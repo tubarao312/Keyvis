a32d912f8c6e5454d7b855d0e9e460d5d629b038
Tiburso <manuelmariamc@gmail.com>
2024-06-01 12:39:50 +0200

    Add user id to session and jwt

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -12,6 +12,20 @@ export const authConfig = {
             // If the user is authorized, return the user object
             return !!auth?.user;
         },
+
+        jwt({ token, user }) {
+            // Add the user id to the token
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+
+        session({ session, token }) {
+            // Add the user id to the session
+            session.user.id = token.id as string;
+            return session;
+        },
     },
     providers: [],
 } satisfies NextAuthConfig;
